f062073073b22a15274d8233f37fb2ff9ae81b38
Pat Kelly <Pat@Mac-mini.local>
2016-02-27 10:28:23 -0500

    Added 2D Array

---
+++ Array2D.swift
@@ -0,0 +1,30 @@
+//
+//  Array2D.swift
+//  CookieCrunch
+//
+//  Created by Pat Kelly on 2/27/16.
+//  Copyright © 2016 Pat Kelly. All rights reserved.
+//
+
+import Foundation
+
+struct Array2D<T> {
+    let columns: Int
+    let rows: Int
+    private var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        array = Array<T?>(count: rows*columns, repeatedValue: nil)
+    }
+   
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[row*columns + column]
+        }
+        set {
+            array[row*columns + column] = newValue
+        }
+    }
+}
